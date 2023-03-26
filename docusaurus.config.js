f527d286ddb06ca501ca8919b87dc101d53099c0
milena-kos <75163549+milena-kos@users.noreply.github.com>
2023-03-26 16:32:37 +0300

    maybe deploy to github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
   favicon: 'img/bigNudge.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://milena-kos.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cmmm-modding-guide/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'milena-kos', // Usually your GitHub org/user name.
   projectName: 'cmmm-modding-guide', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
