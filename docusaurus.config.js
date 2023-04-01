f40070ce87ce6baf3a6222685055cb19090b0539
milena-kos <75163549+milena-kos@users.noreply.github.com>
2023-04-01 17:49:57 +0300

    dark theme by default

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,6 +104,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false
+      }
     }),
 };
 
